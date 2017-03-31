cc13161502c4ec5f3a3c765109548db62a1d2aae
Jeanine Kimball <jeanine.kimball@coxautoinc.com>
2017-03-31 13:35:47 -0400

    Mods to Rules

--- js/Rules.js
+++ js/Rules.js
@@ -11,7 +11,7 @@ Runner.Rules.prototype = {
         var text2 = this.add.text(120, 105, jump, {font:"24px Arial", fill: '#ffffff'});
         var score = "Scoring";
         var text3 = this.add.text(100, 165, score, {font: "32px Arial"});
-        var points = "Rollback = +10 points\nManagers = +20 points\nGen4 = +50 points\nBeer = -1 point\nCoins = -3 points\nRFP = -5 points"
+        var points = "Rollback  +10 points\nManagers  +20 points\nGen4  +50 points\nBeer  -1 point\nCoins  -3 points\nRFP  -5 points";
         var text4 = this.add.text(120, 205, points, {font:"24px Arial", fill: '#ffffff'});
 
         this.playButton = this.add.button(420, 330, 'playButton', this.startGame, this, 'buttonOver', 'buttonOut', 'buttonOver');
